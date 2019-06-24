03c9d6030c3283d9d68972c7a8d0976e0fcf8271
adreedbert <adre.edbert@yahoo.com>
2019-06-24 14:24:36 +0200

    1 file modified

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -20,7 +20,6 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
